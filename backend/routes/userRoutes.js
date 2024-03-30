98808df3554a1cc706f04f66744c9ac6a6c51de4
Harsh Ninave <harshninave58@gmail.com>
2024-03-30 23:38:39 +0530

    User Registration created

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,23 +1,36 @@
 import express from 'express';
+import User from '../models/userModel.js';
 // app.use(express.json());
 const router = express.Router();
 
 /*****************************Register user route*****************************/
-router.post('/', function (req, res) {
-  const { username, password } = req.body;
-  if (!username || !password) {
+router.post('/', async function (req, res) {
+  const { email, password } = req.body;
+  if (!email || !password) {
     return res.status(400).json({
-      error: 'Username and password are required',
+      error: 'Email and password are required for registering user',
+    });
+  }
+
+  try {
+    const user = await User.create({ email, password });
+    res.status(200).json({
+      message: 'User Created!',
+      User,
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: 'Internal Server Error',
     });
   }
 });
 
 /*****************************Login user route*****************************/
 router.post('/login', function (req, res) {
-  const { username, password } = req.body;
-  if (!username || !password) {
+  const { email, password } = req.body;
+  if (!email || !password) {
     return res.status(400).json({
-      error: 'Username and password are required',
+      error: 'Email and password are required for login',
     });
   }
 });
