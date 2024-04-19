de001da075875a4c2457fab41c1bf229c120eeaf
Harsh Ninave <harshninave58@gmail.com>
2024-04-19 22:22:07 +0530

    Auth middleware before creating a post

---
+++ backend/middleware/auth.js
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/userModel';
+
+// We're creating a middleware called auth-
+
+const auth = async (req, res, next) => {
+  // Check if the request headers contain the authorization key
+  const { authorization } = req.headers;
+  if (!authorization) {
+    return res.status(401).json({ error: 'Authorized token not found' });
+  }
+
+  // Grab the token from headers (taking the "Bearer" token away)
+  const token = authorization.split(' ')[1];
+  try {
+    // Decode and extract the user id from token
+    const { _id } = jwt.verify(token, process.env.JWT_SECRET);
+    // Save the user in request
+    req.user = await User.findById(_id).select('_id');
+    next();
+  } catch (error) {
+    res.status(401).json({ error: 'Unauthorized' });
+  }
+};
+
+export default auth;
