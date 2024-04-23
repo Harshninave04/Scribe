ff316545c01b1dedb1f091e7e22feff6dae285d1
Harsh Ninave <harshninave58@gmail.com>
2024-04-23 22:36:42 +0530

    Frontend initialized (tailwindcss,react-router-dom,vite-js)

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="m-0 bg-gray-400">
+        <h1 className="bg-red-500 text-white p-5 text-3xl flex justify-center">Scribe</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
