b21748a1a272910f8da788eb482ef4dc02333b75
Harsh Ninave <harshninave58@gmail.com>
2024-04-25 22:11:42 +0530

    Form Data Handling manually

--- frontend/src/pages/user/Login.jsx
+++ frontend/src/pages/user/Login.jsx
@@ -4,13 +4,40 @@ import Alert from '../../components/Alert';
 const Login = () => {
   // Error State
   // eslint-disable-next-line no-unused-vars
-  const [error, setError] = useState(' Ok');
+  const [error, setError] = useState(null);
+
+  // Form Data State
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  // HandleLogin
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log(email);
+    console.log(password);
+    setEmail('');
+    setPassword('');
+  };
   return (
     <section className="card">
       <h1 className="title">Login to your Account</h1>
-      <form>
-        <input type="email" placeholder="Email" className="input" autoFocus />
-        <input type="password" placeholder="Password" className="input" />
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="Email"
+          className="input"
+          autoFocus
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="input"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <button className="btn">Login</button>
       </form>
       {error && <Alert msg={error} />}
